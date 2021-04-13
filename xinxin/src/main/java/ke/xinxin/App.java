41f307214415d76f0ff5afd87ad6622311decd78
OADpyi <buqier754966@126.com>
2021-04-13 15:40:50 +0800

    update

--- xinxin/src/main/java/ke/xinxin/App.java
+++ xinxin/src/main/java/ke/xinxin/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println("test the line");
         System.out.println("I am come");
         System.out.println("hahah come a confilit");
+        System.out.println("7777777777777777");
         System.out.println("hahahhahahahahahah");
     }
 }
