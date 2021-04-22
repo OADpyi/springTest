bf15d2c76287bc69feec7a2558e2e946862f1308
OADpyi <buqier754966@126.com>
2021-04-22 21:06:24 +0800

    update

---
+++ xinxin/src/main/java/ke/xinxin/Appss2.java
@@ -0,0 +1,26 @@
+package ke.xinxin;
+
+/**
+ * Hello world!
+ *
+ */
+public class Appss2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("test the line");
+        System.out.println("I am come");
+        System.out.println("hahah come a confilit");
+        System.out.println("7777777777777777");
+        System.out.println("hahahhahahahahahah");
+        
+        System.out.println(".........");
+        
+        System.out.println("hahahaha");
+        
+        System.out.println("kkkkkkk");
+        
+        System.out.println("I am jiangpeng");
+    }
+}
